12da46e87cedc8c6d8ccbfb24551690b2b2fcea6
Sajid Rahman <sajidjrahman@icloud.com>
2022-04-29 19:42:20 +0200

    login page added

--- src/Pages/Authentications/Login/Login.js
+++ src/Pages/Authentications/Login/Login.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Google from '../../../images/logo/Google.svg';
+import GitHub from '../../../images/logo/GitHub.svg';
 import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-
+        <div id='login'>
+            <div className='login'>
+                <div>
+                    <h1>Welcome back to<br /><span>InventoryStation</span></h1>
+                    <p>Login to continue to your account...</p>
+                </div>
+                <div className='input-login-form text-center'>
+                    <h2>Login</h2>
+                    <p className='signup-link'>
+                        Don't have an account?
+                        <Link to='/sign-up'> Sign Up</Link>
+                    </p>
+                    <div className='social-login text-center'>
+                        <p className='social-login-title'>Login with Social</p>
+                        <button className='mx-auto btn'>
+                            <img width='40px' src={Google} alt="" />
+                            <h6>Continue with Google</h6>
+                        </button>
+                        <button className='mx-auto btn'>
+                            <img width='40px' src={GitHub} alt="" />
+                            <h6>Continue with GitHub</h6>
+                        </button>
+                    </div>
+                    <div className='or-login-method'>
+                        <div></div>
+                        <p className='mx-2 mb-0'>or</p>
+                        <div></div>
+                    </div>
+                    <form>
+                        <p className='login-form-title'>Login with Email & Password</p>
+                        <input type="email" name="email" id="" required placeholder='Email' />
+                        <input type="password" name="password" id="" required placeholder='Password' />
+                        <p className='forget-password'>Forget Password?</p>
+                        <button className='button-login btn'>Login</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Authentications/SignUp/SignUp.js
+++ src/Pages/Authentications/SignUp/SignUp.js
@@ -6,41 +6,42 @@ import './SignUp.css';
 
 const SignUp = () => {
     return (
-        <div className='sign-up'>
-            <div>
-                <h1>Welcome to<br /><span>InventoryStation</span></h1>
-                <p>Sign In to continue to your account...</p>
-            </div>
-            <div className='input-form text-center'>
-                <h2>Sign Up</h2>
-                <p className='login-link'>
-                    Already have an account?
-                    <Link to='login'> Login</Link>
-                </p>
-                <div className='social-signup text-center'>
-                    <button className='mx-auto btn'>
-                        <img width='40px' src={Google} alt="" />
-                        <h6>Continue with Google</h6>
-                    </button>
-                    <button className='mx-auto btn'>
-                        <img width='40px' src={GitHub} alt="" />
-                        <h6>Continue with GitHub</h6>
-                    </button>
+        <div id='sign-up'>
+            <div className='sign-up'>
+                <div>
+                    <h1>Welcome to<br /><span>InventoryStation</span></h1>
+                    <p>Sign Up to create your account...</p>
                 </div>
-                <div className='or-method'>
-                    <div></div>
-                    <p className='mx-2 mb-0'>or</p>
-                    <div></div>
+                <div className='input-form text-center'>
+                    <h2>Sign Up</h2>
+                    <p className='login-link'>
+                        Already have an account?
+                        <Link to='/login'> Login</Link>
+                    </p>
+                    <div className='social-signup text-center'>
+                        <p className='social-signup-title'>Sign Up with Social</p>
+                        <button className='mx-auto btn'>
+                            <img width='40px' src={Google} alt="" />
+                            <h6>Continue with Google</h6>
+                        </button>
+                        <button className='mx-auto btn'>
+                            <img width='40px' src={GitHub} alt="" />
+                            <h6>Continue with GitHub</h6>
+                        </button>
+                    </div>
+                    <div className='or-signup-method'>
+                        <div></div>
+                        <p className='mx-2 mb-0'>or</p>
+                        <div></div>
+                    </div>
+                    <form>
+                        <p className='signup-form-title'>Sign Up with Email & Password</p>
+                        <input type="text" name="name" id="" placeholder='Name' />
+                        <input type="email" name="email" id="" required placeholder='Email' />
+                        <input type="password" name="password" id="" required placeholder='Password' />
+                        <button className='button-signup btn'>Sign Up</button>
+                    </form>
                 </div>
-                <form>
-                    <input type="text" name="name" id="" placeholder='Name' />
-                    <br />
-                    <input type="email" name="email" id="" required placeholder='Email' />
-                    <br />
-                    <input type="password" name="password" id="" required placeholder='Password' />
-                    <br />
-                    <button className='button-signup btn'>Sign Up</button>
-                </form>
             </div>
         </div>
     );
